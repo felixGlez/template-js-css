50cd8fc12ddcdd68b6e143203282fad68fa8babe
felixGlez <felixgonzalezgarcia2010@gmail.com>
2023-11-23 23:26:11 +0100

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,72 @@
+// Mostrar la primera pregunta
+// Seleccionar pregunta y respuestas
+// Almacenar la respuesta del usuario
+// Avanzar a la siguiente pregunta
+//
+
 import { QUESTIONS } from './questions';
 
-for (const question of QUESTIONS) {
-	console.log(question.question);
-}
+// SELECTORES
+const questionElement = document.getElementById('game-question');
+const optionsElement = document.getElementById('question-options');
+const resultsElement = document.getElementById('results');
+
+let round = 0;
+let userResponses = [];
+
+// FUNCIONES
+const displayResults = () => {
+	optionsElement.classList.replace('flex', 'hide');
+	const fragmentElement = document.createDocumentFragment();
+
+	for (let i = 0; i < userResponses.length; i++) {
+		const newQuestion = document.createElement('h2');
+		newQuestion.textContent = userResponses[i].question;
+		const newCorrectAnswer = document.createElement('h3');
+		newCorrectAnswer.textContent = `${userResponses[i].correctAnswer}`;
+		const newUserAnswer = document.createElement('h3');
+		newUserAnswer.textContent = ' ' + userResponses[i].userAnswer;
+		const newDiv = document.createElement('div');
+		newDiv.classList.add('result-box');
+		newDiv.append(newCorrectAnswer, newUserAnswer);
+
+		fragmentElement.append(newQuestion, newDiv);
+	}
+	resultsElement.append(fragmentElement);
+};
+
+const nextQuestion = event => {
+	if (event.target.id === 'question-options') return;
+
+	const currentQuestion = QUESTIONS[round];
+	const selectedAnswer = event.target.textContent;
+
+	userResponses.push({
+		question: currentQuestion.question,
+		userAnswer: selectedAnswer,
+		correctAnswer: currentQuestion.correctAnswer
+	});
+
+	round++;
+
+	if (round < QUESTIONS.length) {
+		displayQuestion();
+	} else {
+		displayResults();
+	}
+
+	console.log(userResponses);
+};
+
+const displayQuestion = () => {
+	const currentQuestion = QUESTIONS[round];
+	questionElement.textContent = currentQuestion.question;
+
+	for (let i = 0; i < optionsElement.children.length; i++) {
+		optionsElement.children[i].textContent = currentQuestion.options[i];
+	}
+};
+
+optionsElement.addEventListener('click', nextQuestion);
+
+displayQuestion();
